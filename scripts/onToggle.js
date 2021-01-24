f29caa9905c6eee8b4001f59e447e761bf5c99ef
L̵͇͝1̸̺̕g̵̠͝ḧ̶͈́t̴͖͑s̸͔̉w̶̱̓o̵̲͛r̶̛̦d <59493580+l1ghtsword@users.noreply.github.com>
2021-01-24 03:28:49 -0500

    Add files via upload
    initial commit

---
+++ scripts/onToggle.js
@@ -0,0 +1,10 @@
+function toggleNav(ref){
+  document.getElementById("nt-nav-menu-button").classList.toggle('active')
+  document.getElementById("nt-sidebar").classList.toggle('active')
+  document.getElementById("nt-sidebar-placeholder").classList.toggle('active')
+}
+function toggleSearch(ref){
+  document.getElementById("nt-title").classList.toggle('active')
+  document.getElementById("nt-media-search").classList.toggle('active')
+  document.getElementById("nt-random-video").classList.toggle('active')
+}
